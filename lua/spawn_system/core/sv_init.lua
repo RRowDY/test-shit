e73b4ad47793f840787b39c511a35fb0dafc14bd
RRowDY <durellwalzer@gmail.com>
2022-10-03 04:50:38 -0700

    removed data funcs, better table usage

--- lua/spawn_system/core/sv_init.lua
+++ lua/spawn_system/core/sv_init.lua
@@ -1,64 +1,46 @@
-local spawnPos, spawnAng
-local directory = "dynamicspawnpoints"
+dynamicSpawning.spawnTable = {}
+local color = dynamicSpawning.colorTable
 
-hook.Add("Initialize", "serverStart", function(spawnPos, spawnAng)
-    local dataLoad = dynamicSpawning.loadData()
+function dynamicSpawning.storeSpawnPosition(ply)
+    local pos = ply:GetPos()
 
-    if !(spawnPos or spawnAng) then 
+    dynamicSpawning.addSpawnPosition(pos)
+end
+
+function dynamicSpawning.addSpawnPosition(pos)
+    if (table.IsEmpty(dynamicSpawning.spawnTable)) then 
+        dynamicSpawning.coloredPrint(color.red, "[SERVER] ", color_white, "Spawn table was empty, ", color.red, tostring(pos), color_white, " was added to the spawn table\n")
+        dynamicSpawning.spawnTable = {pos}
+    else
+        table.insert(dynamicSpawning.spawnTable, pos)
+    end
+end
+
+function dynamicSpawning.getRandomSpawnPosition(spawnPos)
+    spawnPosition = dynamicSpawning.spawnTable[math.random(#dynamicSpawning.spawnTable)]
+    
+    return spawnPosition
+end
+
+function dynamicSpawning.handlePlayerSpawn(ply, spawnPos)
+    if (table.IsEmpty(dynamicSpawning.spawnTable)) then 
+        dynamicSpawning.coloredPrint(color.red, "[SERVER] ", color_white, "Spawn table is empty, falling back to default map spawns\n")
         return
-    else
-        if not dataLoad then 
-            dynamicSpawning.spawnPoints = {}
-        else
-            dynamicSpawning.spawnPoints = dataLoad
-        end
-    end
-end)
-
-if not file.Exists(directory, "DATA") then
-    file.CreateDir(directory)
-end
-
-function dynamicSpawning.saveData()
-    file.Write(directory .. "/spawnpoints" .. ".txt", util.TableToJSON(dynamicSpawning.spawnPoints))
-end
-
-function dynamicSpawning.loadData()
-    local file = directory .. "/spawnpoints" .. ".txt"
-    if not file.Exists(file, "DATA") then return end
-    local fileData = file.Read(file)
-    if not fileData then return end
-
-    return util.JSONToTable(fileData)
-end
-
-local function spawnSet(ply, text)
-    if (string.lower(text) == "/setspawn") then
-        spawnPos = ply:GetPos()
-        spawnAng = ply:GetAngles()
-        coloredPrint(color_white, ply:Nick() .. " has set the new spawn point @ (", Color(255, 100, 100), tostring(spawnPos), color_white, ") (", Color(255, 100, 100), tostring(spawnAng), color_white, ")\n")
-        dynamicSpawning.spawnPoints = {spawnPos, spawnAng}
-        dynamicSpawning.saveData()
-        return ""
     end
 
-    if (string.lower(text) == "/wipespawn") then 
-        spawnPos = nil
-        spawnAng = nil
-        coloredPrint(color_white, ply:Nick() .. " has wiped the spawn point\n")
+    spawnPos = dynamicSpawning.getRandomSpawnPosition()
+
+    ply:SetPos(spawnPos)
+    dynamicSpawning.coloredPrint(color.red, "[SERVER] ", color.green, ply:Nick(), color_white, " has spawned at ", color.red, tostring(spawnPos) .. "\n")
+end
+
+function dynamicSpawning.handleSpawnStore(ply, txt)
+    if (string.lower(txt) == "/addspawn") then 
+        dynamicSpawning.storeSpawnPosition(ply)
+        dynamicSpawning.coloredPrint(color.red, "[SERVER] ", color.green, ply:Nick(), color_white, " added ", color.red, tostring(ply:GetPos()), color_white, " to the spawn table\n")
         return ""
     end
 end
 
-local function playerSpawn(ply)
-    if !(spawnPos or spawnAng) then 
-        return 
-    else
-        ply:SetPos(spawnPos)
-        ply:SetAngles(spawnAng)
-        coloredPrint(color_white, ply:Nick() .. " has successfully spawned @ (", Color(255, 100, 100), tostring(spawnPos), color_white, ") (", Color(255, 100, 100), tostring(spawnAng), color_white, ")\n")
-    end
-end
-
-hook.Add("PlayerSay", "setSpawn", spawnSet)
-hook.Add("PlayerSpawn", "spawnPlayer", playerSpawn)
+hook.Add("PlayerSay", "addSpawn", dynamicSpawning.handleSpawnStore)
+hook.Add("PlayerSpawn", "spawnPlayer", dynamicSpawning.handlePlayerSpawn)
